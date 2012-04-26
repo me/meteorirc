1d590ab8c011640f7cc269f78c1133155504cf6d
Ivan <abmajor7@gmail.com>
2012-04-26 03:24:24 +0200

    Linkify

--- meteorirc.js
+++ meteorirc.js
@@ -38,7 +38,30 @@ if (Meteor.is_client) {
     }
 
     return hours+":"+minutes;
-  });  
+  });
+
+  Handlebars.registerHelper('linkifyText', function(options) {
+    var val = Handlebars._escape(this["text"]);
+
+    return Meteor.ui.chunk(function(){
+
+    var replaceText, replacePattern1, replacePattern2, replacePattern3;
+
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = val.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+    return replacedText;
+    });
+  });
 
   Template.messages.messages = function(){
     return Messages.find({}, {sort: {time: -1}});
